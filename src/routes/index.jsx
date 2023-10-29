b2b7a2402a08a18e4d04cf206388bb1ffb6b493b
Patty <phuizalopez1@gmail.com>
2023-10-29 16:55:13 -0400

    icons

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,68 +1,74 @@
-import Icon from '@mui/material/Icon';
+import Icon from "@mui/material/Icon";
 // Pages- Publics
-import AboutUs from '../pages/landing-pages/AboutUs';
-import Cosmeticos from '../pages/landing-pages/Esika/Cosmeticos';
-import Maquillaje from '../pages/landing-pages/Esika/Maquillaje';
-import Perfumes from '../pages/landing-pages/Esika/Perfumes';
-import ContactUs from '../pages/landing-pages/ContactUs';
-import SignIn from '../pages/landing-pages/SignIn';
+import AboutUs from "../pages/landing-pages/AboutUs";
+import Cosmeticos from "../pages/landing-pages/Esika/Cosmeticos";
+import Maquillaje from "../pages/landing-pages/Esika/Maquillaje";
+import Perfumes from "../pages/landing-pages/Esika/Perfumes";
+import ContactUs from "../pages/landing-pages/ContactUs";
+import SignIn from "../pages/landing-pages/SignIn";
 // Pages Privates
-import ChangePasswordInitial from '../pages/dashboard-pages/auth/ChangePasswordInitial';
-import Main from '../pages/dashboard-pages/Main';
-import AdminRegister from '../pages/dashboard-pages/users/Admin/AdminRegister';
-import AdminList from '../pages/dashboard-pages/users/Admin/AdminList';
-import PermisionRegister from '../pages/dashboard-pages/users/Permision/PermisionRegister';
-import PermisionList from '../pages/dashboard-pages/users/Permision/PermisionList';
-import RoleRegister from '../pages/dashboard-pages/users/Role/RoleRegister';
-import RoleList from '../pages/dashboard-pages/users/Role/RoleList';
-import PegsList from '../pages/dashboard-pages/users/Pegs/PegsList';
-import PegsRegister from '../pages/dashboard-pages/users/Pegs/PegsRegister';
-import ConsultoriaList from '../pages/dashboard-pages/users/Consultoria/ConsultoriaList';
-import ConsultoriaRegister from '../pages/dashboard-pages/users/Consultoria/ConsultoriaRegister';
-import LiderEmpresariaList from '../pages/dashboard-pages/users/LiderEmpresaria/LiderEmpresariaList';
-import LiderEmpresariaRegister from '../pages/dashboard-pages/users/LiderEmpresaria/LiderEmpresariaRegister';
-import SeccionRegister from '../pages/dashboard-pages/Secciones/SecciónRegister';
-import SeccionList from '../pages/dashboard-pages/Secciones/SeccionList';
-import GerenciaRegister from '../pages/dashboard-pages/users/Gerencia/GerenciaRegister';
-import GerenciaList from '../pages/dashboard-pages/users/Gerencia/GerenciaList';
-import SeguimientoActividades from '../pages/dashboard-pages/SeguimientoActividades';
-import Comunicados from '../pages/dashboard-pages/Services/Comunicados';
-import ServiciosAtencion from '../pages/dashboard-pages/Services/ServiciosAtención';
-import Camapañas from '../pages/dashboard-pages/Services/Campañas';
-import Conferencias from '../pages/dashboard-pages/Services/Conferencias';
+import ChangePasswordInitial from "../pages/dashboard-pages/auth/ChangePasswordInitial";
+import Main from "../pages/dashboard-pages/Main";
+import AdminRegister from "../pages/dashboard-pages/users/Admin/AdminRegister";
+import AdminList from "../pages/dashboard-pages/users/Admin/AdminList";
+import PermisionRegister from "../pages/dashboard-pages/users/Permision/PermisionRegister";
+import PermisionList from "../pages/dashboard-pages/users/Permision/PermisionList";
+import RoleRegister from "../pages/dashboard-pages/users/Role/RoleRegister";
+import RoleList from "../pages/dashboard-pages/users/Role/RoleList";
+import PegsList from "../pages/dashboard-pages/users/Pegs/PegsList";
+import PegsRegister from "../pages/dashboard-pages/users/Pegs/PegsRegister";
+import ConsultoriaList from "../pages/dashboard-pages/users/Consultoria/ConsultoriaList";
+import ConsultoriaRegister from "../pages/dashboard-pages/users/Consultoria/ConsultoriaRegister";
+import LiderEmpresariaList from "../pages/dashboard-pages/users/LiderEmpresaria/LiderEmpresariaList";
+import LiderEmpresariaRegister from "../pages/dashboard-pages/users/LiderEmpresaria/LiderEmpresariaRegister";
+import SeccionRegister from "../pages/dashboard-pages/Secciones/SecciónRegister";
+import SeccionList from "../pages/dashboard-pages/Secciones/SeccionList";
+import GerenciaRegister from "../pages/dashboard-pages/users/Gerencia/GerenciaRegister";
+import GerenciaList from "../pages/dashboard-pages/users/Gerencia/GerenciaList";
+import SeguimientoActividades from "../pages/dashboard-pages/SeguimientoActividades";
+import Comunicados from "../pages/dashboard-pages/Services/Comunicados";
+import ServiciosAtencion from "../pages/dashboard-pages/Services/ServiciosAtención";
+import Camapañas from "../pages/dashboard-pages/Services/Campañas";
+import Conferencias from "../pages/dashboard-pages/Services/Conferencias";
 //import Logout from "../Layouts/pages/dashboard-pages/Logout";
-import HomeIcon from '@mui/icons-material/Home';
-import LockIcon from '@mui/icons-material/Lock';
-import PersonIcon from '@mui/icons-material/Person';
-import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount'; // Icono para Roles
-import SecurityIcon from '@mui/icons-material/Security'; // Icono para Administrador
+
+//importaciones de iconos//
+import HomeIcon from "@mui/icons-material/Home";
+import LockIcon from "@mui/icons-material/Lock";
+import PersonIcon from "@mui/icons-material/Person";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount"; // Icono para Roles
+import SecurityIcon from "@mui/icons-material/Security"; // Icono para Administrador
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import HowToRegIcon from "@mui/icons-material/HowToReg";
+import ListIcon from "@mui/icons-material/List";
+
 const publicRoutes = [
   {
-    name: 'Sobre Nosotros',
-    route: '/about-us',
+    name: "Sobre Nosotros",
+    route: "/about-us",
     component: AboutUs,
   },
   {
-    name: 'Belleza Intelectual',
+    name: "Belleza Intelectual",
     columns: 1,
     rowsPerColumn: 2,
     collapse: [
       {
-        name: 'Esika',
+        name: "Esika",
         collapse: [
           {
-            name: 'Cosmeticos',
-            route: '/beauty-products/esika/cosmeticos',
+            name: "Cosmeticos",
+            route: "/beauty-products/esika/cosmeticos",
             component: Cosmeticos,
           },
           {
-            name: 'Maquillaje',
-            route: '/beauty-products/esika/maquillaje',
+            name: "Maquillaje",
+            route: "/beauty-products/esika/maquillaje",
             component: Maquillaje,
           },
           {
-            name: 'Perfumes',
-            route: '/beauty-products/esika/perfumes',
+            name: "Perfumes",
+            route: "/beauty-products/esika/perfumes",
             component: Perfumes,
           },
         ],
@@ -71,18 +77,18 @@ const publicRoutes = [
         name: "L'bel",
         collapse: [
           {
-            name: 'Cosmeticos',
-            route: '/beauty-products/lbel/cosmeticos',
+            name: "Cosmeticos",
+            route: "/beauty-products/lbel/cosmeticos",
             component: Cosmeticos,
           },
         ],
       },
       {
-        name: 'Cyzone',
+        name: "Cyzone",
         collapse: [
           {
-            name: 'Cosmeticos',
-            route: '/beauty-products/cyzone/cosmeticos',
+            name: "Cosmeticos",
+            route: "/beauty-products/cyzone/cosmeticos",
             component: Cosmeticos,
           },
         ],
@@ -90,13 +96,13 @@ const publicRoutes = [
     ],
   },
   {
-    name: 'Contacto',
-    route: '/contact-us',
+    name: "Contacto",
+    route: "/contact-us",
     component: ContactUs,
   },
   {
-    name: 'Iniciar Sesión',
-    route: '/login',
+    name: "Iniciar Sesión",
+    route: "/login",
     component: SignIn,
   },
 ];
@@ -107,118 +113,122 @@ const privateRoutes = [
   //   component: ChangePasswordInitial,
   // },
   {
-    name: 'Inicio',
-    route: '/dashboard',
+    name: "Inicio",
+    route: "/dashboard",
     component: Main,
     icon: <HomeIcon />,
   },
   {
-    name: 'Permiso',
+    name: "Permiso",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Administrador'],
+    roles: ["Administrador"],
     icon: <LockIcon />,
     collapse: [
       {
-        name: 'Registrar',
-        route: '/register-permision',
+        name: "Registrar",
+        route: "/register-permision",
         component: PermisionRegister,
       },
       {
-        name: 'Listar',
-        route: '/list-permision',
+        name: "Listar",
+        route: "/list-permision",
         component: PermisionList,
       },
     ],
   },
   {
-    name: 'Rol',
+    name: "Rol",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Administrador'],
+    roles: ["Administrador"],
     icon: <SupervisorAccountIcon />,
     collapse: [
       {
-        name: 'Registrar',
-        route: '/register-role',
+        name: "Registrar",
+        route: "/register-role",
         component: RoleRegister,
       },
       {
-        name: 'Listar',
-        route: '/list-role',
+        name: "Listar",
+        route: "/list-role",
         component: RoleList,
       },
     ],
   },
   {
-    name: 'Administrador',
+    name: "Administrador",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Administrador'],
+    roles: ["Administrador"],
     icon: <SecurityIcon />, //
     collapse: [
       {
-        name: 'Registrar',
-        route: '/register-admin',
+        name: "Registrar",
+        route: "/register-admin",
         component: AdminRegister,
+        icon: <AddCircleOutlineIcon />, //
       },
       {
-        name: 'Listar',
-        route: '/list-admin',
+        name: "Listar",
+        route: "/list-admin",
         component: AdminList,
+        icon: <ListIcon />, //
       },
     ],
   },
   {
-    name: 'Gerencia',
+    name: "Gerencia",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Administrador'],
+    roles: ["Administrador"],
+    icon: <PersonIcon />,
     collapse: [
       {
-        name: 'Registrar',
-        route: '/register-gerente',
+        name: "Registrar",
+        route: "/register-gerente",
         component: GerenciaRegister,
+        icon: <HowToRegIcon />,
       },
       {
-        name: 'Listar',
-        route: '/list-gerente',
+        name: "Listar",
+        route: "/list-gerente",
         component: GerenciaList,
       },
     ],
   },
   {
-    name: 'Lider Empresa',
+    name: "Lider Empresa",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Gerente', 'Administrador'],
+    roles: ["Gerente", "Administrador"],
     collapse: [
       {
-        name: 'Seccion',
+        name: "Seccion",
         collapse: [
           {
-            name: 'Registar',
-            route: '/registar-seccion',
+            name: "Registar",
+            route: "/registar-seccion",
             component: SeccionRegister,
           },
           {
-            name: 'Listar',
-            route: '/listar-sección',
+            name: "Listar",
+            route: "/listar-sección",
             component: SeccionList,
           },
         ],
       },
       {
-        name: 'Lider Empresaria',
+        name: "Lider Empresaria",
         collapse: [
           {
-            name: 'Registar',
-            route: '/registar-lider-empresa',
+            name: "Registar",
+            route: "/registar-lider-empresa",
             component: LiderEmpresariaRegister,
           },
           {
-            name: 'Listar',
-            route: '/listar-lider-empresa',
+            name: "Listar",
+            route: "/listar-lider-empresa",
             component: LiderEmpresariaList,
           },
         ],
@@ -226,74 +236,74 @@ const privateRoutes = [
     ],
   },
   {
-    name: 'Consultoria',
+    name: "Consultoria",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Lider', 'Gerente'],
+    roles: ["Lider", "Gerente"],
     collapse: [
       {
-        name: 'Registrar',
-        route: '/register-Consultora',
+        name: "Registrar",
+        route: "/register-Consultora",
         component: ConsultoriaRegister,
       },
       {
-        name: 'Listar',
-        route: '/list-consultora',
+        name: "Listar",
+        route: "/list-consultora",
         component: ConsultoriaList,
       },
     ],
   },
   {
-    name: 'Pegs',
+    name: "Pegs",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Lider', 'Gerente', 'Administrador'],
+    roles: ["Lider", "Gerente", "Administrador"],
     collapse: [
       {
-        name: 'Registrar',
-        route: '/register-pegs',
+        name: "Registrar",
+        route: "/register-pegs",
         component: PegsRegister,
       },
       {
-        name: 'Listar',
-        route: '/list-pegs',
+        name: "Listar",
+        route: "/list-pegs",
         component: PegsList,
       },
     ],
   },
   {
-    name: 'Servicios',
+    name: "Servicios",
     columns: 1,
     rowsPerColumn: 2,
-    roles: ['Lider', 'Gerente'],
+    roles: ["Lider", "Gerente"],
     collapse: [
       {
-        name: 'Conferencias',
-        route: '/conferencias',
+        name: "Conferencias",
+        route: "/conferencias",
         component: Conferencias,
       },
       {
-        name: 'Campañas',
-        route: '/campanias',
+        name: "Campañas",
+        route: "/campanias",
         component: Camapañas,
       },
       {
-        name: 'Servicios de Atención',
-        route: '/servicios-de-atencion',
+        name: "Servicios de Atención",
+        route: "/servicios-de-atencion",
         component: ServiciosAtencion,
       },
       {
-        name: 'Comunicados',
-        route: '/comunicados',
+        name: "Comunicados",
+        route: "/comunicados",
         component: Comunicados,
-        roles: ['Lider,', 'Gerente', 'Consultoria', 'Pegs'],
+        roles: ["Lider,", "Gerente", "Consultoria", "Pegs"],
       },
     ],
   },
   {
-    name: 'Seguimiento de Actividades',
-    route: '/seguimiento-de-actividades',
-    roles: ['Gerente'],
+    name: "Seguimiento de Actividades",
+    route: "/seguimiento-de-actividades",
+    roles: ["Gerente"],
     component: SeguimientoActividades,
   },
 ];
